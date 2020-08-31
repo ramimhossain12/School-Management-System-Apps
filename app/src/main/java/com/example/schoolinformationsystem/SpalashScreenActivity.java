a78fcda8e87e1e929014fc0be7649bd6430a5b48
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-01 01:22:48 +0600

    Splash screen add

---
+++ app/src/main/java/com/example/schoolinformationsystem/SpalashScreenActivity.java
@@ -0,0 +1,59 @@
+package com.example.schoolinformationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SpalashScreenActivity extends AppCompatActivity {
+
+
+    private ProgressBar progressBar;
+    int progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_spalash_screen);
+
+//progressbar
+        progressBar = findViewById(R.id.progreesbarID);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+                startApp();
+            }
+        });
+        thread.start();
+
+    }
+
+    private void startApp() {
+
+        //progressbar finish
+        Intent intent= new Intent(SpalashScreenActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void doWork() {
+
+        for (progress=20;progress<=100;progress=progress+20){
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
