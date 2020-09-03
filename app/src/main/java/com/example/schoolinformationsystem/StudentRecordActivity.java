bcef1bd11dbeafff531dc213330d3d206f3c8386
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-04 00:49:22 +0600

    Bug fixing

--- app/src/main/java/com/example/schoolinformationsystem/StudentRecordActivity.java
+++ app/src/main/java/com/example/schoolinformationsystem/StudentRecordActivity.java
@@ -2,41 +2,90 @@ package com.example.schoolinformationsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class StudentRecordActivity extends AppCompatActivity  implements View.OnClickListener {
+public class StudentRecordActivity extends AppCompatActivity   {
 
     private EditText name,email,pass;
-    private Button button;
+    private Button save,fetch,remove,clear;
     private TextView textView;
 
+    String Name = "Name Key"; String Email = "Email Key"; String Result = "Result Key";
+
+    SharedPreferences sp;
+    SharedPreferences.Editor ed;
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_record);
 
         name = findViewById(R.id.nameID);
         email = findViewById(R.id.emailID);
         pass = findViewById(R.id.passID);
-        button= findViewById(R.id.btnID);
+        save= findViewById(R.id.btnID);
+        fetch = findViewById(R.id.fetchID);
+        remove = findViewById(R.id.removeID);
+        clear = findViewById(R.id.clearID);
         textView = findViewById(R.id.ttID);
-        button.setOnClickListener(this);
+
+
+
+        sp = getSharedPreferences("Sasasushiq", Context.MODE_PRIVATE);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ed = sp.edit();
+                ed.putString(Name, name.getText().toString());
+                ed.putString(Email, email.getText().toString());
+                ed.putString(Result, pass.getText().toString());
+                ed.apply();
+            }
+        });
+
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sp.contains(Name))
+                    name.setText(sp.getString(Name, ""));
+                if (sp.contains(Email))
+                    email.setText(sp.getString(Email, ""));
+                if (sp.contains(Result))
+                    pass.setText(sp.getString(Result ,""));
+
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name.setText("");
+                email.setText("");
+                pass.setText("");
+            }
+        });
+
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ed.remove(Name);
+                ed.remove(Email);
+                ed.remove(Result);
+                ed.apply();
+            }
+        });
 
     }
 
-    @Override
-    public void onClick(View v) {
-        String name1 = name.getText().toString();
-        String email1 = email.getText().toString();
-        String pass1 = pass.getText().toString();
 
-        if (v.getId()==R.id.btnID){
-
-            textView.setText("Name: "+name1+"\nEmail: "+email1+"\nResult: "+pass1);
-        }
-    }
 }
